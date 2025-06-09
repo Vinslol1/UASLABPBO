51425aec909f579f05c0889dbbc26b8226a10387
PatrickNW <patrickdwangsa@gmail.com>
2025-06-09 17:30:43 +0700

    Functuonal Login&Register

--- BukanPeduliLindungi/src/main/java/com/pos/bukanpedulilindungi/HelloController.java
+++ BukanPeduliLindungi/src/main/java/com/pos/bukanpedulilindungi/HelloController.java
@@ -70,16 +70,94 @@ public class HelloController implements Initializable {
 
     private AlertMessage alert = new AlertMessage();
 
+    public void loginAccount() {
+
+        if (login_username.getText().isEmpty()
+                || login_password.getText().isEmpty()) {
+            alert.errorMessage("Incorrect Username/Password");
+        } else {
+
+            String sql = "SELECT * FROM admin WHERE username = ? AND password = ?";
+
+            connect = Database.connectDB();
+
+            try {
+
+                if (!login_showPassword.isVisible()) {
+                    if (!login_showPassword.getText().equals(login_password.getText())) {
+                        login_showPassword.setText(login_password.getText());
+                    }
+                } else {
+                    if (!login_showPassword.getText().equals(login_password.getText())) {
+                        login_password.setText(login_showPassword.getText());
+                    }
+                }
+
+                prepare = connect.prepareStatement(sql);
+                prepare.setString(1, login_username.getText());
+                prepare.setString(2, login_password.getText());
+                result = prepare.executeQuery();
+
+                if (result.next()) {
+                    alert.successMessage("Login Successfully");
+                } else {
+                    alert.errorMessage("Incorrect Username/Password");
+                }
+//                if (result.next()) {
+//                    // TO GET THE USERNAME
+//                    Data.admin_username = login_username.getText();
+//                    Data.admin_id = Integer.parseInt(result.getString("admin_id"));
+//
+//                    // IF CORRECT USERNAME AND PASSWORD
+//                    alert.successMessage("Login Successfully!");
+//
+//                    // LINK MAIN FORM FOR ADMIN
+//                    Parent root = FXMLLoader.load(getClass().getResource("AdminMainForm.fxml"));
+//                    Stage stage = new Stage();
+//
+//                    stage.setTitle("Hospital Management System | Admin Portal");
+//                    stage.setScene(new Scene(root));
+//                    stage.show();
+//
+//                    // TO HIDE YOUR ADMIN PAGE (LOGIN FORM)
+//                    login_loginBtn.getScene().getWindow().hide();
+//                } else {
+//                    // IF WRONG USERNAME OR PASSWORD
+//                    alert.errorMessage("Incorrect Username/Password");
+//                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public void loginShowPassword() {
+
+        if (login_checkBox.isSelected()) {
+            login_showPassword.setText(login_password.getText());
+            login_showPassword.setVisible(true);
+            login_password.setVisible(false);
+        } else {
+            login_password.setText(login_showPassword.getText());
+            login_showPassword.setVisible(false);
+            login_password.setVisible(true);
+        }
+
+    }
+
     public void registerAccount() {
 
         if (register_email.getText().isEmpty()
                 || register_username.getText().isEmpty()
                 || register_password.getText().isEmpty()) {
-            // LETS CREATE OUR ALERT MESSAGE
+            // KALO KOSONG FIELD NYA
             alert.errorMessage("Please fill all blank fields");
         } else {
 
-            // WE WILL CHECK IF THE USERNAME THAT USER ENTERED IS ALREADY EXIST TO OUR DATABASE
+            // MEMERIKSA APAKAH USERNAME YANG DIMASUKKAN OLEH USER SUDAH ADA DI DATABASE
             String checkUsername = "SELECT * FROM admin WHERE username = '"
                     + register_username.getText() + "'";
 
@@ -102,7 +180,7 @@ public class HelloController implements Initializable {
 
                 if (result.next()) {
                     alert.errorMessage(register_username.getText() + " is already exist!");
-                } else if (register_password.getText().length() < 8) { // CHECK IF THE CHARACTERS OF THE PASSWORD IS LESS THAN TO 8
+                } else if (register_password.getText().length() < 8) { // CEK JIKA PASSWORD MEMILIKI KARAKTER DIBAWAH 8
                     alert.errorMessage("Invalid Password, at least 8 characters needed");
                 } else {
                     // TO INSERT THE DATA TO OUR DATABASE
@@ -120,11 +198,11 @@ public class HelloController implements Initializable {
                     prepare.executeUpdate();
 
                     alert.successMessage("Registered Successfully!");
-//                    registerClear();
-//
-//                    // TO SWITCH THE FORM INTO LOGIN FORM
-//                    login_form.setVisible(true);
-//                    register_form.setVisible(false);
+                    registerClear();
+
+                    // UNTUK MENGGANTI FORM MENJADI LOGIN FORM
+                    login_form.setVisible(true);
+                    register_form.setVisible(false);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -132,6 +210,30 @@ public class HelloController implements Initializable {
         }
     }
 
+    // INI SIH FUNCTION YANG AKAN CLEAR FORM REGISTER SIAP DIGUNAKAN
+    public void registerClear() {
+        register_email.clear();
+        register_username.clear();
+        register_password.clear();
+        register_showPassword.clear();
+    }
+
+    // FUNCTION UNTUK SHOW PASSWORD NYA DARI CHECKBOX
+    public void registerShowPassword() {
+
+        if (register_checkBox.isSelected()) {
+            register_showPassword.setText(register_password.getText());
+            register_showPassword.setVisible(true);
+            register_password.setVisible(false);
+        } else {
+            register_password.setText(register_showPassword.getText());
+            register_showPassword.setVisible(false);
+            register_password.setVisible(true);
+        }
+
+    }
+
+// Tombol Pindah Dari login ke register atau sebaliknya dari Link paling bawah
     public void switchForm(ActionEvent event){
         if(event.getSource() == login_registerHere) {
             // register form akan muncul :))
