830b70fcda9c0245bcbd0a3b6db31aa592525575
PatrickNW <patrickdwangsa@gmail.com>
2025-06-09 16:01:16 +0700

    Experiment Library

--- BukanPeduliLindungi/src/main/java/com/pos/bukanpedulilindungi/HelloController.java
+++ BukanPeduliLindungi/src/main/java/com/pos/bukanpedulilindungi/HelloController.java
@@ -67,6 +67,68 @@ public class HelloController implements Initializable {
     private PreparedStatement prepare;
     private ResultSet result;
 
+    public void registerAccount() {
+
+        if (register_email.getText().isEmpty()
+                || register_username.getText().isEmpty()
+                || register_password.getText().isEmpty()) {
+            // LETS CREATE OUR ALERT MESSAGE
+            alert.errorMessage("Please fill all blank fields");
+        } else {
+
+            // WE WILL CHECK IF THE USERNAME THAT USER ENTERED IS ALREADY EXIST TO OUR DATABASE
+            String checkUsername = "SELECT * FROM admin WHERE username = '"
+                    + register_username.getText() + "'";
+
+            connect = Database.connectDB();
+
+            try {
+
+                if (!register_showPassword.isVisible()) {
+                    if (!register_showPassword.getText().equals(register_password.getText())) {
+                        register_showPassword.setText(register_password.getText());
+                    }
+                } else {
+                    if (!register_showPassword.getText().equals(register_password.getText())) {
+                        register_password.setText(register_showPassword.getText());
+                    }
+                }
+
+                prepare = connect.prepareStatement(checkUsername);
+                result = prepare.executeQuery();
+
+                if (result.next()) {
+                    alert.errorMessage(register_username.getText() + " is already exist!");
+                } else if (register_password.getText().length() < 8) { // CHECK IF THE CHARACTERS OF THE PASSWORD IS LESS THAN TO 8
+                    alert.errorMessage("Invalid Password, at least 8 characters needed");
+                } else {
+                    // TO INSERT THE DATA TO OUR DATABASE
+                    String insertData = "INSERT INTO admin (email, username, password, date) VALUES(?,?,?,?)";
+
+                    Date date = new Date();
+                    java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+
+                    prepare = connect.prepareStatement(insertData);
+                    prepare.setString(1, register_email.getText());
+                    prepare.setString(2, register_username.getText());
+                    prepare.setString(3, register_password.getText());
+                    prepare.setString(4, String.valueOf(sqlDate));
+
+                    prepare.executeUpdate();
+
+                    alert.successMessage("Registered Successfully!");
+                    registerClear();
+
+                    // TO SWITCH THE FORM INTO LOGIN FORM
+                    login_form.setVisible(true);
+                    register_form.setVisible(false);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void switchForm(ActionEvent event){
         if(event.getSource() == login_registerHere) {
             // register form akan muncul :))
